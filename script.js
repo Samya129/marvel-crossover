e3c4183e3fa457469f5e3919f54ee7c105798661
Sam129 <Samyaf020@gmail.com>
2020-12-15 20:33:27 -0500

    Team updated html and created images file together.

---
+++ script.js
@@ -0,0 +1,26 @@
+//Front-end
+//Implement boostrap into our design
+
+$("#search-button").on("click", function() {
+    alert("Works?");
+  });
+
+
+
+
+
+
+
+//Back-End
+//Movie api and Marvel api link to our application
+//search bar
+//search button
+//an for loop or for each array of characters with its own buttons
+//timeline
+//module of each character so we avoid different pages with a hover feature
+//dark and light toggle
+//opacity changes as you view a character
+
+
+
+
